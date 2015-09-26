664b1c0adc027a5b7bcb0fb92f3faf93359fa1a1
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-26 17:43:33 +0300

    fix search/Generator

--- generators/search/Generator.php
+++ generators/search/Generator.php
@@ -2,7 +2,9 @@
 
 namespace yii\gii\plus\generators\search;
 
-use yii\gii\generators\model\Generator as YiiGiiModelGenerator;
+use yii\gii\CodeFile,
+    Yii,
+    yii\gii\generators\model\Generator as YiiGiiModelGenerator;
 
 
 class Generator extends YiiGiiModelGenerator
@@ -11,5 +13,51 @@ class Generator extends YiiGiiModelGenerator
     public function getName()
     {
         return 'Search Model Generator';
+    }
+
+    public function getDescription()
+    {
+        return '';
+    }
+
+    public function attributes()
+    {
+        $attributes = array_diff(parent::attributes(), ['controllerClass', 'viewPath', 'baseControllerClass', 'indexWidgetType']);
+        $key = array_search('searchModelClass', $attributes);
+        if ($key !== false) {
+            $attributes[$key] = 'newModelClass';
+        }
+        return $attributes;
+    }
+
+    public function rules()
+    {
+        $attributes = $this->attributes();
+        $rules = [['newModelClass', 'required']];
+        foreach (parent::rules() as $rule) {
+            if (!is_array($rule[0])) {
+                $rule[0] = [$rule[0]];
+            }
+            $key = array_search('searchModelClass', $rule[0]);
+            if ($key !== false) {
+                $rule[0][$key] = 'newModelClass';
+            }
+            $rule[0] = array_intersect($rule[0], $attributes);
+            if (count($rule[0])) {
+                $rules[] = $rule;
+            }
+        }
+        return $rules;
+    }
+
+    public function requiredTemplates()
+    {
+        return ['search.php'];
+    }
+
+    public function generate()
+    {
+        $newModel = Yii::getAlias('@' . str_replace('\\', '/', ltrim($this->newModelClass, '\\') . '.php'));
+        return [new CodeFile($newModel, $this->render('search.php'))];
     }
 }
---
+++ generators/search/default/search.php
@@ -0,0 +1,5 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\plus\generators\search\Generator */
+
+echo '<?php' . "\n";
