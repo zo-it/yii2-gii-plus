6e938776d1bd29ac61442c097aec4f166249335e
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-26 10:22:42 +0300

    add init(), defaultTemplate(), render()

--- generators/base/model/Generator.php
+++ generators/base/model/Generator.php
@@ -2,14 +2,35 @@
 
 namespace yii\gii\plus\generators\base\model;
 
-use yii\gii\generators\model\Generator as YiiGiiModelGenerator;
+use ReflectionClass,
+    yii\gii\generators\model\Generator as YiiGiiModelGenerator;
 
 
 class Generator extends YiiGiiModelGenerator
 {
 
+    public $use = 'Yii';
+
     public function getName()
     {
         return 'Base Model Generator';
+    }
+
+    public function init()
+    {
+        parent::init();
+        $this->use = array_filter(array_map('trim', explode(',', $this->use)), 'strlen');
+    }
+
+    public function defaultTemplate()
+    {
+        $class = new ReflectionClass('yii\gii\generators\model\Generator');
+        return dirname($class->getFileName()) . '/default';
+    }
+
+    public function render($template, $params = [])
+    {
+        $useDirective = 'use ' . implode(',' . "\n" . '    ', $this->use) . ';';
+        return str_replace('use Yii;', $useDirective, parent::render($template, $params));
     }
 }
