f223a679a2a5287e1baa4e507f860a50c7cec4d8
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-26 12:33:47 +0300

    fix search/Generator

--- generators/base/search/Generator.php
+++ generators/base/search/Generator.php
@@ -2,14 +2,38 @@
 
 namespace yii\gii\plus\generators\base\search;
 
-use yii\gii\generators\model\Generator as YiiGiiModelGenerator;
+use yii\gii\generators\crud\Generator as YiiGiiCrudGenerator;
 
 
-class Generator extends YiiGiiModelGenerator
+class Generator extends YiiGiiCrudGenerator
 {
+
+    public $searchModelClass;
 
     public function getName()
     {
         return 'Base Search Model Generator';
+    }
+
+    public function getDescription()
+    {
+        return '';
+    }
+
+    public function attributes()
+    {
+        return array_diff(parent::attributes(), ['controllerClass', 'viewPath', 'baseControllerClass', 'indexWidgetType']);
+    }
+
+    public function rules()
+    {
+        $rules = [['searchModelClass', 'required']];
+        foreach (parent::rules() as $rule) {
+            $rule[0] = array_diff((array)$rule[0], ['controllerClass', 'viewPath', 'baseControllerClass', 'indexWidgetType']);
+            if (count($rule[0])) {
+                $rules[] = $rule;
+            }
+        }
+        return $rules;
     }
 }
