4690f8af4b9eae07b73d0e2946f397b4d71de398
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-26 09:35:06 +0300

    add coreGenerators()

--- Module.php
+++ Module.php
@@ -7,4 +7,14 @@ use yii\gii\Module as YiiGiiModule;
 
 class Module extends YiiGiiModule
 {
+
+    protected function coreGenerators()
+    {
+        return array_merge(parent::coreGenerators(), [
+            'base-model' => ['class' => 'yii\gii\plus\generators\base\model\Generator'],
+            'model' => ['class' => 'yii\gii\plus\generators\model\Generator'],
+            'base-search' => ['class' => 'yii\gii\plus\generators\base\search\Generator'],
+            'search' => ['class' => 'yii\gii\plus\generators\search\Generator']
+        ]);
+    }
 }
