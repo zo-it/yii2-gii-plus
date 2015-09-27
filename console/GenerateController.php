01e827eb2088642b8091c72a093b3282c5c51d3b
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-27 18:26:33 +0300

    fix 3

--- console/GenerateController.php
+++ console/GenerateController.php
@@ -3,24 +3,21 @@
 namespace yii\gii\plus\console;
 
 use yii\console\Controller,
-PDO,
-Yii;
+    PDO,
+    Yii;
 
 
 class GenerateController extends Controller
 {
 
-public function actionShowTables()
-{
-$db = Yii::$app->getDb();
-var_dump($db->getIsActive());
-$driverName = $db->pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
-var_dump($driverName);
-
-foreach ($db->createCommand('SHOW TABLES;')->queryAll(PDO::FETCH_NUM) as $row) {
-var_dump($row);
-}
-}
+    public function actionShowTables()
+    {
+        $db = Yii::$app->getDb();
+        $db->open();
+        $driverName = $db->pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
+        var_dump($driverName);
+        var_dump($db->createCommand('SHOW TABLES;')->queryColumn());
+    }
 
     public function actionIndex()
     {
