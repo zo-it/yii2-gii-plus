d430e0f830123882aaf4c971c155cdb4c33c2d66
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-29 07:33:39 +0300

    add FormHelper

--- generators/base/search/form.php
+++ generators/base/search/form.php
@@ -1,10 +1,15 @@
 <?php
 
+use yii\jui\AutoComplete,
+    yii\gii\plus\helpers\FormHelper;
+
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\plus\generators\base\search\Generator */
 /* @var $form yii\widgets\ActiveForm */
 
-echo $form->field($generator, 'modelClass');
+echo $form->field($generator, 'modelClass')->widget(AutoComplete::classname(), [
+    'clientOptions' => ['source' => []]
+]);
 echo $form->field($generator, 'searchModelClass');
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
